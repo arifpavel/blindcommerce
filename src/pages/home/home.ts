18209b15eb2e46a853a93ec25897157214ab57bc
Arif Pavel <pavelpatiya@gmail.com>
2019-01-27 00:14:20 +0600

    initial commit with lazyloading pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  search:string = 'Search Product';
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  ionViewDidLoad() {
+    console.log(this.search);
+    console.log('ionViewDidLoad HomePage');
+  }
+
+  gotoProduct(){
+    this.navCtrl.push('ProductPage');
+  }
+
+  searchProduct(productname){
+    console.log(this.search)
+    console.log(this.navParams.data)
+
+  }
 }
